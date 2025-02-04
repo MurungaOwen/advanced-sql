a3cb37d0b99aff74997589724c8ae0c58b769f06
= <=>
2025-02-04 10:07:19 +0300

    first task creatin users table

--- task1.sql
+++ task1.sql
@@ -2,6 +2,6 @@
 CREATE TABLE IF NOT EXISTS users(
     id int AUTO_INCREMENT PRIMARY KEY,
     name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
+    email varchar(255) UNIQUE NOT NULL,
     city ENUM("US", "CO", "IN") NOT NULL DEFAULT 'US'
 );
