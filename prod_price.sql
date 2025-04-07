9e7f9d6836d19882e7a2e6a5dd731e01c21df36e
0x01 <122882250+MurungaOwen@users.noreply.github.com>
2025-04-07 12:51:20 +0300

    DDL definitions

--- prod_price.sql
+++ prod_price.sql
@@ -12,6 +12,21 @@
  
 
 -- Write a solution to find the prices of all products on 2019-08-16. Assume the price of all products before any change is 10.
+CREATE TABLE Products (
+    product_id INT NOT NULL,
+    new_price DECIMAL(10, 2) NOT NULL,
+    change_date DATE NOT NULL,
+    PRIMARY KEY (product_id, change_date)
+);
+
+INSERT INTO Products (product_id, new_price, change_date) VALUES
+(1, 20, '2019-08-14'),
+(2, 50, '2019-08-14'),
+(1, 30, '2019-08-15'),
+(1, 5,  '2019-08-16'),
+(2, 65, '2019-08-17'),
+(3, 20, '2019-08-18');
+
 WITH data AS(
     SELECT
         product_id,
