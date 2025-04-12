c6977c41afb0c41b2045e083a6368791319f06fe
0x01 <122882250+MurungaOwen@users.noreply.github.com>
2025-04-12 14:12:29 +0300

    solution using substring

--- substrings.sql
+++ substrings.sql
@@ -9,3 +9,19 @@ INSERT INTO Users (user_id, name)
 VALUES 
     (1, 'aLice'),
     (2, 'bOB');
+
+-- Step 3: Query to fix names (First letter uppercase, rest lowercase)
+-- What does substring do?
+--     heres the working SUBSTRING(word, start, end)
+--     works like array slicing
+--     UPPER -> converts to uppercase
+--     LOWER -> converts to lowercase
+    
+SELECT 
+    user_id,
+    CONCAT(
+        UPPER(SUBSTRING(name, 1, 1)), 
+        LOWER(SUBSTRING(name, 2))
+    ) AS name
+FROM Users
+ORDER BY user_id;
